10c12744f8da42fd2322c1a3d1d46672408ed511
Riad Mohamed <riaduk1@gmail.com>
2018-10-12 18:47:58 +0200

    Lecture 3.

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -13,9 +13,9 @@ struct Card {
     var isMatched = false
     var identifier: Int
     
-    static var identifierFactory = 0
+    private static var identifierFactory = 0
     
-    static func getUniquieIdentifier () -> Int {
+    private static func getUniquieIdentifier () -> Int {
         identifierFactory += 1
         return identifierFactory
     }
